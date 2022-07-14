acd97b0dae00290ecc724e4d3eb6d4e09ec6c20a
Yana Ivanova <yana@plam.ch>
2022-07-14 20:55:41 +0300

    fix scss imports

--- client/src/features/counter/Counter.tsx
+++ client/src/features/counter/Counter.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
 import { decrement, increment, incrementByAmount, incrementAsync, incrementIfOdd, selectCount } from './counterSlice'
-import styles from './Counter.module.css'
+import styles from './Counter.module.scss'
 
 export function Counter() {
     const count = useAppSelector(selectCount)
